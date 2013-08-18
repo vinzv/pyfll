a76d01036053bdf3f32727d626b47aa8cc359f04
Alf Gaida <agaida@siduction.org>
2013-08-18 22:07:22 +0200

    xperimental configurations newly created

---
+++ bootstrap-xperimental.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+CODENAME="Thunderstruck"
+CODENAME_SAFE=thunderstruck
+VERSION=13.1.1
+
+cd pyfll/templates
+for TEMPLATE in *xperimental*; do
+sed -e "s/\@CODENAME\@/${CODENAME}/g" \
+    -e "s/\@CODENAME_SAFE\@/${CODENAME_SAFE}/g" \
+    -e "s/\@VERSION\@/${VERSION}/g" \
+      $TEMPLATE \
+       > ../${TEMPLATE%\.*} 
+done
