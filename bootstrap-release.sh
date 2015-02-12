fbed6d61807a29e5968d6d67e3711f372155cacd
Alf Gaida <agaida@siduction.org>
2015-02-12 01:09:56 +0100

    release script - we now have a generator for current and release configurations based on current templates

--- bootstrap-release.sh
+++ bootstrap-release.sh
@@ -2,15 +2,18 @@
 
 set -e
 
-CODENAME="White Room"
-CODENAME_SAFE="whiteroom"
+CODENAME="Indian Summer"
+CODENAME_SAFE="indiansummer"
 VERSION="15.1.0"
 
 cd pyfll/templates
 for TEMPLATE in *current*; do
+TARGET=`echo ${TEMPLATE} | sed  "s/current/${CODENAME_SAFE}/g "`
+echo "Target: ${TARGET}"
+
 sed -e "s/\@CODENAME\@/${CODENAME}/g" \
     -e "s/\@CODENAME_SAFE\@/${CODENAME_SAFE}/g" \
     -e "s/\@VERSION\@/${VERSION}/g" \
       ${TEMPLATE} \
-       > ../conf/${TEMPLATE} 
+       > ../conf/${TARGET}
 done
