c94eaab6317b6de8378367f313c514eb8b32d0a0
Alf Gaida <agaida@siduction.org>
2015-08-25 23:29:46 +0200

    new bootstrap scripts

--- bootstrap-release.sh
+++ bootstrap-release.sh
@@ -4,16 +4,28 @@ set -e
 
 CODENAME="Indian Summer"
 CODENAME_SAFE="indiansummer"
+FLAVOURS="cinnamon gnome kde lxde lxqt mate nox xfce xorg"
+RELEASE="indiansummer"
 VERSION="15.1.0"
 
-cd pyfll/templates
-for TEMPLATE in *current*; do
-TARGET=`echo ${TEMPLATE} | sed  "s/current/${CODENAME_SAFE}/g "`
-echo "Target: ${TARGET}"
 
-sed -e "s/\@CODENAME\@/${CODENAME}/g" \
-    -e "s/\@CODENAME_SAFE\@/${CODENAME_SAFE}/g" \
-    -e "s/\@VERSION\@/${VERSION}/g" \
-      ${TEMPLATE} \
-       > ../conf/${TARGET}
+cd pyfll/templates
+for FLAVOUR in ${FLAVOURS};  do
+    for ARCH  in amd64 i386; do
+        case $ARCH in
+            "amd64") KERNEL="amd64"
+            ;;
+            "i386") KERNEL="686"
+            ;;
+        esac
+        sed -e "s/\@ARCH\@/${ARCH}/g" \
+            -e "s/\@CODENAME\@/${CODENAME}/g" \
+            -e "s/\@CODENAME_SAFE\@/${CODENAME_SAFE}/g" \
+            -e "s/\@FLAVOUR\@/${FLAVOUR}/g" \
+            -e "s/\@KERNEL\@/${KERNEL}/g" \
+            -e "s/\@RELEASE\@/${RELEASE}/g" \
+            -e "s/\@VERSION\@/${VERSION}/g" \
+             ${RELEASE}.conf \
+             > ../conf/${FLAVOUR}_${RELEASE}_${ARCH}.conf
+   done
 done
--- bootstrap-xperimental.sh
+++ bootstrap-xperimental.sh
@@ -4,13 +4,28 @@ set -e
 
 CODENAME="Thunderstruck"
 CODENAME_SAFE="thunderstruck"
+FLAVOURS="cinnamon gnome kde lxde lxqt mate nox xfce xorg"
+RELEASE="xperimental"
 VERSION="15.1.0"
 
+
 cd pyfll/templates
-for TEMPLATE in *xperimental*; do
-sed -e "s/\@CODENAME\@/${CODENAME}/g" \
-    -e "s/\@CODENAME_SAFE\@/${CODENAME_SAFE}/g" \
-    -e "s/\@VERSION\@/${VERSION}/g" \
-      ${TEMPLATE} \
-       > ../conf/${TEMPLATE} 
+for FLAVOUR in ${FLAVOURS};  do
+    for ARCH  in amd64 i386; do
+        case $ARCH in
+            "amd64") KERNEL="amd64"
+            ;;
+            "i386") KERNEL="686"
+            ;;
+        esac
+        sed -e "s/\@ARCH\@/${ARCH}/g" \
+            -e "s/\@CODENAME\@/${CODENAME}/g" \
+            -e "s/\@CODENAME_SAFE\@/${CODENAME_SAFE}/g" \
+            -e "s/\@FLAVOUR\@/${FLAVOUR}/g" \
+            -e "s/\@KERNEL\@/${KERNEL}/g" \
+            -e "s/\@RELEASE\@/${RELEASE}/g" \
+            -e "s/\@VERSION\@/${VERSION}/g" \
+             ${RELEASE}.conf \
+             > ../conf/${FLAVOUR}_${RELEASE}_${ARCH}.conf
+   done
 done
